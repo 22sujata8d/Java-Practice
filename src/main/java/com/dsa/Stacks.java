66899e39e534ae755029404487c6054248b38f09
Sujata Dwivedi <sujata8dwivedi@gmail.com>
2022-05-03 18:06:15 +0530

    feat: add stack examples

---
+++ src/main/java/com/dsa/Stacks.java
@@ -0,0 +1,20 @@
+package com.dsa;
+
+import java.util.Stack;
+
+public class Stacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+
+        System.out.println(stack.empty());
+    }
+}
