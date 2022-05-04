52a2edba153d4c805d86d67ded81e76cbdd49ebf
Sujata Dwivedi <sujata8dwivedi@gmail.com>
2022-05-04 07:01:42 +0530

    feat: add deque examples using linked list class

--- src/main/java/com/dsa/Deques.java
+++ src/main/java/com/dsa/Deques.java
@@ -1,10 +1,25 @@
 package com.dsa;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class Deques {
     public static void main(String[] args) {
-        Deque<String> deque = new LinkedList<>();
+        Deque<Person> deque = new LinkedList<>();
+        deque.add(new Person("Alex"));
+        deque.add(new Person("Merlin"));
+        deque.add(new Person("Clara"));
+        deque.addFirst(new Person("Robert"));
+        deque.addLast(new Person("Will"));
 
+        System.out.println(deque);
+        System.out.println(deque.size());
+
+        deque.removeFirst();
+        System.out.println(deque);
+        deque.removeLast();
+        System.out.println(deque);
     }
+
+    record Person(String name){}
 }
